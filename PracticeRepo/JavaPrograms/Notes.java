5bb6f935b6c178a7e39e397e0d82b9c3a036df58
Ganesh Sharya <150882158+PawarGaneshNayak@users.noreply.github.com>
2024-06-12 18:48:50 +0530

    Update Notes.java

--- PracticeRepo/JavaPrograms/Notes.java
+++ PracticeRepo/JavaPrograms/Notes.java
@@ -3,7 +3,7 @@ class Public
 {
     public static void main(String[] args)
     {
-		System.out.println("Hello World");
+	    System.out.println("Hello World");
         System.out.println("This is my first program");
     }
 }
