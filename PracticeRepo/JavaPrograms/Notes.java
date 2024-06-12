0498cf176cb1da5d6b9129f9725662a388c3a032
Ganesh Sharya <150882158+PawarGaneshNayak@users.noreply.github.com>
2024-06-12 18:48:04 +0530

    Update Notes.java

--- PracticeRepo/JavaPrograms/Notes.java
+++ PracticeRepo/JavaPrograms/Notes.java
@@ -1,7 +1,8 @@
 package usage;		
 class Public 
 {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
 		System.out.println("Hello World");
         System.out.println("This is my first program");
     }
