6b985643dcd0f938d93e5e350a834875bd28e63a
Ganesh Sharya <150882158+PawarGaneshNayak@users.noreply.github.com>
2024-06-14 00:22:04 +0530

    Updated MultiplicationTableOfGivenNumber.java

--- PracticeRepo/JavaPrograms/DailyPrograms/MultiplicationTableOfGivenNumber.java
+++ PracticeRepo/JavaPrograms/DailyPrograms/MultiplicationTableOfGivenNumber.java
@@ -1,1 +1,12 @@
 /* Write a program to print Multiplication table of a given number... */
+
+  
+public class MultiplicationTableOfGivenNumber
+{
+    System.out.println("Enter a number to print table");
+    int num = new  java.util.Scanner(System.in).nextInt();
+    for( int i = 1; i<=10; i++)
+    {
+        System.out.println(num +" * "+ i +" = " + ( num*i) );
+    }
+}
