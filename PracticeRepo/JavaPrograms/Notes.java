a137eddc7bc228a1d9d4275743758de405c2bc4f
Ganesh Sharya <150882158+PawarGaneshNayak@users.noreply.github.com>
2024-06-14 00:13:05 +0530

    Update and rename Notes.java to Java-Notes-SSSIT.text

--- PracticeRepo/JavaPrograms/Notes.java
+++
@@ -1,8 +0,0 @@
-package usage;		
-class Public 
-{
-    public static void main(String[] args)    {
-	    System.out.println("Hello World");
-        System.out.println("This is my first program");
-    }
-}
